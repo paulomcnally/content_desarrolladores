528e3dd067ec2c0ecf35619e30fcec1e1f003f6c
paulomcnally <paulomcnally@gmail.com>
2013-08-07 15:45:14 -0600

    remove curl

--- post-receive.sh
+++ post-receive.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 cd /var/www/html/desarrolladores.com.ni/content_desarrolladores
 git pull
-
-CT="Content-Type:text/html"
-TEST="curl http://localhost:1934/update -H $CT"
-echo $TEST
-RESPONSE=`$TEST`
-echo $RESPONSE
